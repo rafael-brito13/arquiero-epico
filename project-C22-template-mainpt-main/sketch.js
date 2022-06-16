08550024a180badc2caa192e9a1d7c1f038324e1
rafael-brito13 <102705276+rafael-brito13@users.noreply.github.com>
2022-06-15 19:48:04 -0500

    Add files via upload

---
+++ project-C22-template-mainpt-main/sketch.js
@@ -0,0 +1,48 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var palyer, playerBase, playerArcher;
+var baseimage;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+  angleMode(DEGREES);
+
+  //criar corpo da base do jogador
+
+  //criar corpo do jogador
+
+
+
+}
+
+function draw() {
+  background(backgroundImg);
+
+  //exibir a imagem do jogador usando a função image()
+
+
+  //exibir a imagem da base do jogador usando a função image()
+
+
+  Engine.update(engine);
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("ARQUEIRO ÉPICO", width / 2, 100);
+}
